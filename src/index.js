1464df9ab8e76318f179380ef985c40fc069e9bd
ozor <ozor@ngs.ru>
2020-10-03 12:14:22 +0700

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+    return (array && array.length > 0) ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return (array && array.length > 0) ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ( !array || array.length === 0 ) {
+        return 0;
+    }
+
+    var sum = 0;
+    for (n in array) {
+        sum += parseInt(array[n]);
+    }
+
+    return parseFloat(sum / array.length);
 }
